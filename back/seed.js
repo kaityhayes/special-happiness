4267e21a1bf24bd8bc46719e55c69141de29e652
Steve McLaughlin <stevejrmc@gmail.com>
2023-03-27 22:47:54 +0100

    Modified schema and seed--actors and genre cast to string

--- back/seed.js
+++ back/seed.js
@@ -2,41 +2,41 @@ const MoviesSeed = [
   {
     title: "A Quiet Place",
     rating: "7.5",
-    actors: ["Emily Blunt", "John Krasinski"],
+    actor: "Emily Blunt",
     release: "March 9, 2018",
-    genre: ["Sci-fi", "Horror"],
+    genre: "Horror",
     image: "https://upload.wikimedia.org/wikipedia/en/a/a0/A_Quiet_Place_film_poster.png"
   },
   {
     title: "Ready Player One",
     rating: "7.4",
-    actors: ["Tye Sheridan", "Olivia Cook", "Ben Mendelsohn"],
+    actor: "Tye Sheridan",
     release: "March 29, 2018",
-    genre: ["Sci-fi"],
+    genre: "Sci-fi",
     image: "https://upload.wikimedia.org/wikipedia/en/7/74/Ready_Player_One_%28film%29.png"
   },
   {
     title: "Star Wars: The Last Jedi",
     rating: "6.9",
-    actors: ["Daisy Ridley", "Mark Hamil", "John Boyega"],
+    actor: "Daisy Ridley",
     release: "December 15, 2017",
-    genre: ["Sci-fi"],
+    genre: "Sci-fi",
     image: "https://upload.wikimedia.org/wikipedia/en/7/7f/Star_Wars_The_Last_Jedi.jpg"
   },
   {
     title: "Ghostbusters: Afterlife",
     rating: "7.1",
-    actors: ["Paul Rudd", "Bill Murray", "Dan Aykroyd", "Mckenna Grace"],
+    actor: "Paul Rudd",
     release: "August 23, 2021",
-    genre: ["Sci-fi", "Comedy"],
+    genre: "Comedy",
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Ghostbusters_-_Afterlife_official_theatrical_poster.jpg/220px-Ghostbusters_-_Afterlife_official_theatrical_poster.jpg"
   },
   {
     title: "Up",
     rating: "8.3",
-    actors: ["Ed Asner", "Jordan Negal", "Delroy Lindo"],
+    actor: "Ed Asner",
     release: "May 13, 2009",
-    genre: ["family", "Comedy", "Animated"],
+    genre: "family",
     image: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Up_%282009_film%29.jpg/220px-Up_%282009_film%29.jpg"
   }
 ];
